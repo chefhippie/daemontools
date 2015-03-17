eb2f23d45be5f17566af9f50542813bb8e508e03
Thomas Boerger <tboerger@suse.de>
2015-03-17 11:46:25 +0100

    Refresh the repos after added

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ when "suse"
     key node["daemontools"]["zypper"]["key"]
     title node["daemontools"]["zypper"]["title"]
 
-    action :add
+    action [:add, :refresh]
 
     only_if do
       node["daemontools"]["zypper"]["enabled"]
